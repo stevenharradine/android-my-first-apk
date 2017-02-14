f406495ae9d3ecdc7ee0e53b184fb80ed93dfabc
Steven Harradine <stevenharradine@gmail.com>
2017-02-14 18:33:44 -0500

    inital commit

---
+++ app/src/main/java/foss4you/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package foss4you.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
